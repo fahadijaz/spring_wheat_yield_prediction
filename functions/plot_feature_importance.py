b21344e5842e560ed7df10daec46454011469d2a
fahadijaz <fahadijaz@outlook.com>
2021-10-13 22:35:42 +0200

    Checkpoint: Original DH-DM Integration + weather data
    Original DH-DM Simps and Trapz Integration + Average DH-DM where not available + Weather data + Zero entries in Masbasis pga DHDM

--- functions/plot_feature_importance.py
+++ functions/plot_feature_importance.py
@@ -1,9 +1,41 @@
-def plot_feature_importance(importance,names,model_type):
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import os
+import math
+import datetime
+import numpy as np
+import pandas as pd
+from copy import copy
+from tqdm import tqdm
+
+# Dictionaries
+import json
+from pprint import pprint
+
+# Iterate in loops
+import itertools
+from itertools import zip_longest
+
+# Simpsons integration
+from numpy import trapz
+from scipy.integrate import simps
+
+# Visualisation
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# To display df nicely in loops
+from IPython.display import display 
+
+
+def plot_feature_importance(importance, names, model):
 
     #Create arrays from feature importance and feature names
     feature_importance = np.array(importance)
     feature_names = np.array(names)
-
+    model_type = str(model).split('(')[0]
     #Create a DataFrame using a Dictionary
     data={'feature_names':feature_names,'feature_importance':feature_importance}
     fi_df = pd.DataFrame(data)
@@ -19,5 +51,7 @@ def plot_feature_importance(importance,names,model_type):
     plt.title(model_type + 'Feature Importance')
     plt.xlabel('Feature Importance')
     plt.ylabel('Feature Names')
-    plt.savefig('Data/feature_importance_'+model_type+'.pdf',dpi=500, bbox_inches='tight')
+    # plt.savefig('Data/feature_importance_'+model_type+'.pdf',dpi=500, bbox_inches='tight')
+    plt.show()
     display(fi_df)
+    
--- functions/test_data_regression.py
+++ functions/test_data_regression.py
@@ -1,3 +1,21 @@
+from functions.plot_feature_importance import plot_feature_importance
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import os
+import math
+import datetime
+import numpy as np
+import pandas as pd
+
+
+# Visualisation
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 
 #==============================================================================
 # Defining the function to vaiidate the model with the test data and 
@@ -5,12 +23,44 @@
 #==============================================================================
 pred = []
 acc = []
-import pprint as pprint
-import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
 
-def test_data_regression(model, features, X_train, X_train_std, y_train, X_test, X_test_std, y_test):
+    
+
+def test_train_split(X, y, split_, rs = 100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_, random_state=rs)
+    return X_train, X_test, y_train, y_test
+
+def std_scaler(X_train, X_test):
+
+def train_pred_model(model,  X_train, y_train, X_test, y_test):
+    temp_model = model
+    temp_model.fit(X_train, y_train)
+    y_pred = temp_model.predict(X_test)
+    y_pred_train = temp_model.predict(X_train)
+    
+    return temp_model, y_pred, y_pred_train
+
+
+def feature_imp(model):
+success = False
+while not success:
+    try:
+        importances = model.feature_importances_
+        success = True
+        plot_feature_importance(importance,features,model)
+    except:
+        importances = None
+        pass
+    
+def rmse_():
+
+def r2_score_():
+
+
+def plot_feat_imp(importance, features, model):
+
+
+def test_data_regression():
     pred = []
     accuracy = {}
     accuracy_std = {}
@@ -29,6 +79,7 @@ def test_data_regression(model, features, X_train, X_train_std, y_train, X_test,
         try:
             importances = model.feature_importances_
             success = True
+            plot_feature_importance(importance,features,model)
         except:
             importances = None
             pass
@@ -37,14 +88,15 @@ def test_data_regression(model, features, X_train, X_train_std, y_train, X_test,
     # model.fit(X_train_std, y_train)
     # y_pred_std = model.predict(X_test_std)
     
-    success_std = False
-    while not success_std:
-        try:
-            importances_std = model.feature_importances_
-            success_std = True
-        except:
-            importances_std = None
-            pass
+    # success_std = False
+    # while not success_std:
+     #    try:
+      #       importances_std = model.feature_importances_
+       #      success_std = True
+            
+    #     except:
+    #         importances_std = None
+    #         pass
         
     #==============================================================================
     # Compute performance
@@ -52,7 +104,7 @@ def test_data_regression(model, features, X_train, X_train_std, y_train, X_test,
     
     mse = mean_squared_error(y_test, y_pred, squared=True)
     #mse_std = mean_squared_error(y_test, y_pred_std, squared=True)
-    mse_train = mean_squared_error(y_test, y_pred_train, squared=True)
+    mse_train = mean_squared_error(y_train, y_pred_train, squared=True)
 
     accuracy['MSE'] = mse
     #accuracy_std['MSE'] = mse_std
@@ -60,7 +112,7 @@ def test_data_regression(model, features, X_train, X_train_std, y_train, X_test,
 
     rmse = mean_squared_error(y_test, y_pred, squared=False)
     #rmse_std = mean_squared_error(y_test, y_pred_std, squared=False)
-    rmse_train = mean_squared_error(y_test, y_pred_train, squared=False)
+    rmse_train = mean_squared_error(y_train, y_pred_train, squared=False)
 
     accuracy['RMSE'] = rmse
     #accuracy_std['RMSE'] = rmse_std
@@ -68,15 +120,16 @@ def test_data_regression(model, features, X_train, X_train_std, y_train, X_test,
 
     r2 = r2_score(y_test, y_pred)
     #r2_std = r2_score(y_test, y_pred_std)
-    r2_train = r2_score(y_test, y_pred_train)
+    r2_train = r2_score(y_train, y_pred_train)
 
     accuracy['R2 Score'] = r2
     #accuracy_std['R2 Score'] = r2_std
     accuracy_train['R2 Score'] = r2_train
 
-    acc.append(accuracy)
+    # acc.append(accuracy)
     # Print accuracy computed from predictions on the test set
     pp = pprint.PrettyPrinter(indent=4, width=80, depth=None, stream=None, compact=True, sort_dicts=False)
     pp.pprint(accuracy)
-    
-    return accuracy, accuracy_std, importances, importances_std 
+    pp.pprint(accuracy_train)
+
+    return accuracy, accuracy_train, importances
